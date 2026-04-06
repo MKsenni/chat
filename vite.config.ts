dba93872c15dff937c5ef0693d58e7db7f0291f6
k.merkulova <k.merkulova@studyworld.io>
2026-04-06 20:35:40 +0500

    fix: fix base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chat/',
+  base: '/chat',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
